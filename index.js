7cf25f729b1f32d9b44eb910dc6e07ec43357d2b
beatsbydan <danielonyeachonam2020@gmail.com>
2022-11-01 10:53:36 +0100

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const firstName = document.getElementById("firstname")
 const lastName = document.getElementById("lastname")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
-// const regexMail;
+const regexMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 // const regexPass;
 //setting the error message
 const setError = (id, message) => {
