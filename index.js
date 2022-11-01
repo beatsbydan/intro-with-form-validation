562f6e0399280736404dd1d2285b6f10638b4eaa
beatsbydan <danielonyeachonam2020@gmail.com>
2022-11-01 06:45:35 +0100

    updates

---
+++ index.js
@@ -0,0 +1,18 @@
+//Form validation
+//requirements
+const submit = document.getElementById("submit")
+console.log(submit)
+const firstName = document.getElementById("firstname")
+const lastName = document.getElementById("lastname")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+// const regexMail;
+// const regexPass;
+//setting the error message
+const setError = (id, message) => {
+    
+}
+
+submit.addEventListener("click", () => {
+    console.log("Lovely, progrss is being made")
+})
